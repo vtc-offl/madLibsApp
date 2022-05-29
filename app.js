bf475e4a9f87b24239b7414d7c14a35229a7fdfb
Trulok Chandar <theviper.chandar@gmail.com>
2022-05-29 19:57:29 +0530

    add

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ function updateName() {
 var myDiv = document.getElementById("clickMe");
 myDiv.innerHTML = "I like" + "ba" + (+"Javascript") + "a";
 let questions = prompt("No.of questions?");
+let questionCounter = 0;
 let userInputs = [];
-for(let i = questions; i >= 0; i--)
+for(let i = questions; i > 0; i--)
 {
     let questionArray = prompt('Enter a verb/noun');
     userInputs.push(questionArray);
+    questionCounter+=1;
+    //console.log(questionArray[questionCounter] + `; (${questionTotal} questions left)`);
 }
     
 console.log(`In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`);
